c12527ac61eb475840b49355d4f1bb3c34bafe1d
Akash R Prabhu <akashprabhu41@gmail.com>
2022-08-09 12:25:29 +0530

    2 rows added as components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
+
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl="" />
-      <Row title="Trending Now"/>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,41 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-function Row ({title}) {
-    const [movies, setMovies] = useState([]);
+  //snippet of code which runs on specific condition
+  useEffect(() => {
+    //if [] is blank it mean run once when the component is rendered an ddont run again . otherwise pass a variable like [movies]
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
   return (
-    <div>
-        {/* Title */}
-        <h1>{title}</h1>
-        {/* container with porters inside */}
+    <div className="row">
+      {/* Title */}
+      <h1>{title}</h1>
+
+      {/* container with porters inside */}
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            className="row_poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Row;
